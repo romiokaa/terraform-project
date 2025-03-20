ebe5f2545744dfaf81f41597b418043977e7aa7c
romiokaa <maissaa.hachi@gmail.com>
2025-03-20 16:29:03 +0100

    fichier main

---
+++ main.tf
@@ -0,0 +1,55 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "flask-vnet"
+  address_space        = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "flask-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "vm_ip" {
+  name                = "flask-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "flask-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  network_interface_id = azurerm_subnet.subnet.id
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id         = azurerm_public_ip.vm_ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "flask-vm"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"
+    version   = "latest"
+  }
+}
--- outputs.tf
+++
@@ -1,3 +0,0 @@
-output "vm_ip" {
-  value = azurerm_public_ip.vm_ip.ip_address
-}
