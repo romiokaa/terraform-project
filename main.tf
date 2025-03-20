8d1387c49b71a60ea52ce3b787b39d2d77bc82fb
romiokaa <maissaa.hachi@gmail.com>
2025-03-20 17:45:35 +0100

    test

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "mon-resource-group"
+  location = var.location
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "flask-vnet"
-  address_space        = ["10.0.0.0/16"]
-  location            = var.location
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
@@ -14,31 +19,32 @@ resource "azurerm_subnet" "subnet" {
 
 resource "azurerm_public_ip" "vm_ip" {
   name                = "flask-public-ip"
-  location            = var.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "nic" {
   name                = "flask-nic"
-  location            = var.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  network_interface_id = azurerm_subnet.subnet.id
+
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id         = azurerm_public_ip.vm_ip.id
+    public_ip_address_id          = azurerm_public_ip.vm_ip.id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "flask-vm"
-  location            = var.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   size                = var.vm_size
   admin_username      = var.admin_username
   admin_password      = var.admin_password
+  disable_password_authentication = false
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
@@ -51,5 +57,34 @@ resource "azurerm_linux_virtual_machine" "vm" {
     offer     = "UbuntuServer"
     sku       = "20.04-LTS"
     version   = "latest"
+  }
+}
+
+resource "random_string" "suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "flaskstorage${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "flask-container"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.2"
+    }
   }
 }
